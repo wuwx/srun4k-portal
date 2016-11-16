0ee5455bd7532b50f9c817373af01f7931a16790
Wu Weixin <wuweixin@gmail.com>
2016-11-16 10:44:20 +0800

    设置正确的 Laravel 位置

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,19 @@
 <html ng-app lang="en">
 <head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta http-equiv="pragma" content="no-cache" />
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <script>
-    window.Laravel = { csrfToken: '{{ csrf_token() }}' };
-  </script>
-  <title>IP控制网关</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/css/app.css" type="text/css" rel="stylesheet" media="all" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>IP控制网关</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/app.css" type="text/css" rel="stylesheet" media="all" />
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
 </head>
 
 <body class="bottom">
