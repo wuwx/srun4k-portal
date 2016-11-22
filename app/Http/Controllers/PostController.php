afb22986f5a25fa27c6e12b811bdb2024b3fb206
Wu Weixin <wuweixin@gmail.com>
2016-11-22 14:33:16 +0800

    cache json data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Response;
 
 class PostController extends Controller
@@ -13,7 +14,10 @@ class PostController extends Controller
             case "html":
                 return "";
             case "json":
-                $json = file_get_contents("http://network.neu.edu.cn/api/get_recent_posts/");
+                if (!$json = Cache::get('json')) {
+                    $json = file_get_contents("http://network.neu.edu.cn/api/get_recent_posts/");
+                    Cache::put('json', $json, 10);
+                }
                 return Response::make($json, 200, [
                     'Content-Type' => "application/json; charset=UTF-8",
                 ]);
